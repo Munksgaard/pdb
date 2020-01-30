193949bac7f56a26dee34b462887134bd9276011
Philip Munksgaard <philip@munksgaard.me>
2020-01-30 16:33:30 +0100

    Make repl a little bit more robust
    With these changes, it doesn't crash every time something doesn't parse.

--- src/repl.rs
+++ src/repl.rs
@@ -21,42 +21,48 @@ where
         let mut line = String::new();
         reader.read_line(&mut line)?;
 
-        let ast = parse(&line)?;
-
-        match ast {
-            Statement::Create(ident, def) => {
-                tables.push((ident, def, Vec::new()));
-                writer.write_all(b"Created\n")?;
-                writer.flush()?;
-            }
-            Statement::Insert(ident, expr) => {
-                if let Some((_, def, objs)) =
-                    tables.iter_mut().find(|(ident2, _, _)| ident2 == &ident)
-                {
-                    if unify(&expr, &def.ty) {
-                        let result = eval(expr);
-                        objs.push(result);
-                        writer.write_all(b"Inserted 1\n")?;
+        match parse(&line) {
+            Ok(ast) => match ast {
+                Statement::Create(ident, def) => {
+                    tables.push((ident, def, Vec::new()));
+                    writer.write_all(b"Created\n")?;
+                    writer.flush()?;
+                }
+                Statement::Insert(ident, expr) => {
+                    if let Some((_, def, objs)) =
+                        tables.iter_mut().find(|(ident2, _, _)| ident2 == &ident)
+                    {
+                        if unify(&expr, &def.ty) {
+                            let result = eval(expr);
+                            objs.push(result);
+                            writer.write_all(b"Inserted 1\n")?;
+                        } else {
+                            writer.write_all(
+                                format!(
+                                    "Could not insert {:?} into table {:?} with definition {:?}\n",
+                                    expr, ident, &def.ty
+                                )
+                                .as_bytes(),
+                            )?;
+                        }
                     } else {
-                        writer.write_all(
-                            format!(
-                                "Could not insert {:?} into table {:?} with definition {:?}\n",
-                                expr, ident, &def.ty
-                            )
-                            .as_bytes(),
-                        )?;
+                        writer.write_all(b"No such table\n")?;
                     }
-                } else {
-                    writer.write_all(b"No such table")?;
+                    writer.flush()?;
                 }
-                writer.flush()?;
-            }
-            Statement::Select(ident) => {
-                if let Some((_, _, objs)) = tables.iter().find(|(ident2, _, _)| ident2 == &ident) {
-                    writer.write_all(format!("{:?}\n", objs).as_bytes())?;
-                } else {
-                    writer.write_all(b"No such table")?;
+                Statement::Select(ident) => {
+                    if let Some((_, _, objs)) =
+                        tables.iter().find(|(ident2, _, _)| ident2 == &ident)
+                    {
+                        writer.write_all(format!("{:?}\n", objs).as_bytes())?;
+                    } else {
+                        writer.write_all(b"No such table\n")?;
+                    }
+                    writer.flush()?;
                 }
+            },
+            Err(e) => {
+                writer.write_all(format!("No parse: {}\n", e).as_bytes())?;
                 writer.flush()?;
             }
         }
