5a7f7e4cd621fcfe0d0ded941b77ed290a30af90
Philip Munksgaard <philip@munksgaard.me>
2020-10-12 16:22:45 +0200

    Add rustyline for a better REPL experience

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,11 @@
 use anyhow::Result;
 use pdb;
-use std::io;
 
 fn main() -> Result<()> {
     println!("Hello! This is pdb");
     println!("Feel free to type in commands");
 
-    let stdin = io::stdin();
-    let stdout = io::stdout();
-
-    pdb::repl::start(&mut stdin.lock(), &mut stdout.lock())
-        .unwrap_or_else(|e| panic!("An error occurred: {}", e));
+    pdb::repl::start().unwrap_or_else(|e| panic!("An error occurred: {}", e));
 
     Ok(())
 }
--- src/repl.rs
+++ src/repl.rs
@@ -3,75 +3,66 @@ use crate::eval::eval;
 use crate::object::Object;
 use crate::parse::parse;
 use crate::ty::matches_type;
-use anyhow::Result;
-use std::io::{BufRead, Write};
+use anyhow::{anyhow, Error, Result};
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
 
-const PROMPT: &[u8; 3] = b">> ";
+const PROMPT: &str = ">> ";
 
-pub fn start<R, W>(reader: &mut R, writer: &mut W) -> Result<()>
-where
-    R: BufRead,
-    W: Write,
-{
+pub fn start() -> Result<()> {
     let mut tables: Vec<(Ident, TableDefinition, Vec<Object>)> = Vec::new();
 
+    let mut rl = Editor::<()>::new();
     loop {
-        writer.write_all(PROMPT)?;
-        writer.flush()?;
-
-        let mut line = String::new();
-        reader.read_line(&mut line)?;
-
-        if &line == "" {
-            writer.write_all(b"\n")?;
-            writer.flush()?;
-            return Ok(());
-        }
-
-        match parse(&line) {
-            Ok(ast) => match ast {
-                Statement::Create(ident, def) => {
-                    tables.push((ident, def, Vec::new()));
-                    writer.write_all(b"Created\n")?;
-                    writer.flush()?;
-                }
-                Statement::Insert(ident, expr) => {
-                    if let Some((_, def, objs)) =
-                        tables.iter_mut().find(|(ident2, _, _)| ident2 == &ident)
-                    {
-                        if matches_type(&expr, &def.ty) {
-                            let result = eval(expr);
-                            objs.push(result);
-                            writer.write_all(b"Inserted 1\n")?;
-                        } else {
-                            writer.write_all(
-                                format!(
-                                    "Could not insert {:?} into table {:?} with definition {:?}\n",
-                                    expr, ident, &def.ty
-                                )
-                                .as_bytes(),
-                            )?;
+        let readline = rl.readline(PROMPT);
+        match readline {
+            Ok(line) => match parse(&line) {
+                Ok(ast) => {
+                    match ast {
+                        Statement::Create(ident, def) => {
+                            tables.push((ident, def, Vec::new()));
+                            println!("Created\n");
                         }
-                    } else {
-                        writer.write_all(b"No such table\n")?;
+                        Statement::Insert(ident, expr) => {
+                            if let Some((_, def, objs)) =
+                                tables.iter_mut().find(|(ident2, _, _)| ident2 == &ident)
+                            {
+                                if matches_type(&expr, &def.ty) {
+                                    let result = eval(expr);
+                                    objs.push(result);
+                                    println!("Inserted 1\n");
+                                } else {
+                                    println!("Could not insert {:?} into table {:?} with definition {:?}\n",
+                                             expr, ident, &def.ty
+                                    );
+                                }
+                            } else {
+                                println!("No such table\n");
+                            }
+                        }
+                        Statement::Select(ident) => {
+                            if let Some((_, _, objs)) =
+                                tables.iter().find(|(ident2, _, _)| ident2 == &ident)
+                            {
+                                println!("{:?}\n", objs);
+                            } else {
+                                println!("No such table\n");
+                            }
+                        }
                     }
-                    writer.flush()?;
+                    rl.add_history_entry(line.as_str());
                 }
-                Statement::Select(ident) => {
-                    if let Some((_, _, objs)) =
-                        tables.iter().find(|(ident2, _, _)| ident2 == &ident)
-                    {
-                        writer.write_all(format!("{:?}\n", objs).as_bytes())?;
-                    } else {
-                        writer.write_all(b"No such table\n")?;
-                    }
-                    writer.flush()?;
+                Err(e) => {
+                    println!("No parse: {}\n", e);
                 }
             },
-            Err(e) => {
-                writer.write_all(format!("No parse: {}\n", e).as_bytes())?;
-                writer.flush()?;
+            Err(ReadlineError::Interrupted) => {
+                break Err(anyhow!("unimplemented"));
             }
+            Err(ReadlineError::Eof) => {
+                return Ok(());
+            }
+            Err(err) => break Err(Error::new(err)),
         }
     }
 }
