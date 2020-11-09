bb1b7c4b18d2f78851d577c20098886ccccfac58
Philip Munksgaard <philip@munksgaard.me>
2020-11-09 14:26:40 +0100

    Separate eval_stm from start...

--- src/db.rs
+++ src/db.rs
@@ -10,16 +10,85 @@ use std::sync::mpsc::{Receiver, Sender};
 
 type Tables = Vec<(Ident, TableDefinition, Vec<Object>)>;
 
-struct Env {
+pub struct Env {
     ty_env: ty::Env,
     env: Environment,
+    tables: Tables,
+}
+
+pub fn eval_stm(env: &mut Env, stm: Statement) -> Result<String> {
+    match stm {
+        Statement::Create(ident, def) => {
+            env.tables.push((ident, def, Vec::new()));
+            Ok(String::from("Created\n"))
+        }
+        Statement::Insert(ident, expr) => {
+            if let Some((_, def, objs)) = env
+                .tables
+                .iter_mut()
+                .find(|(ident2, _, _)| ident2 == &ident)
+            {
+                // infer type of expr and try to unify with def.ty
+                let ty = ty::infer(
+                    &mut HashMap::new(),
+                    &mut NameSource::new(),
+                    &env.ty_env,
+                    &expr,
+                )
+                .map_err(|e| anyhow!("{}", e))?;
+
+                if ty::unify(std::iter::once((ty.clone(), def.ty.clone()))).all(|x| x.is_ok()) {
+                    let result = eval(&env.env, expr)?;
+                    objs.push(result);
+                    Ok(String::from("Inserted 1\n"))
+                } else {
+                    Err(anyhow!(
+                        "Could not insert {:?} into table {:?} with definition {:?}\n",
+                        expr,
+                        ident,
+                        &def.ty
+                    ))
+                }
+            } else {
+                Err(anyhow!("No such table\n"))
+            }
+        }
+        Statement::Select(ident) => {
+            if let Some((_, _, objs)) = env.tables.iter().find(|(ident2, _, _)| ident2 == &ident) {
+                Ok(format!("{:?}\n", objs))
+            } else {
+                Err(anyhow!("No such table\n"))
+            }
+        }
+        Statement::Let(ident, expr) => {
+            // infer type of expr and try to unify with def.ty
+            let ty = ty::infer(
+                &mut HashMap::new(),
+                &mut NameSource::new(),
+                &env.ty_env,
+                &expr,
+            )
+            .map_err(|e| anyhow!("{}", e))
+            .unwrap();
+
+            let scheme = ty::generalize(&env.ty_env, ty.clone());
+
+            env.ty_env.insert(ident.clone(), scheme);
+
+            let obj = eval(&env.env, expr)?;
+
+            env.env = env.env.insert(&ident, obj);
+
+            Ok(format!("{}: {}\n", ident, ty))
+        }
+    }
 }
 
 pub fn start(rx: Receiver<(Statement, Sender<Result<String>>)>) -> Result<()> {
-    let mut tables: Tables = Vec::new();
     let mut env = Env {
         ty_env: HashMap::new(),
         env: Environment::new(),
+        tables: Vec::new(),
     };
 
     loop {
@@ -31,76 +100,9 @@ pub fn start(rx: Receiver<(Statement, Sender<Result<String>>)>) -> Result<()> {
             }
         };
 
-        match stm {
-            Statement::Create(ident, def) => {
-                tables.push((ident, def, Vec::new()));
-                tx.send(Ok(String::from("Created\n")))
-                    .context("Ack channel prematurely closed")?;
-            }
-            Statement::Insert(ident, expr) => {
-                if let Some((_, def, objs)) =
-                    tables.iter_mut().find(|(ident2, _, _)| ident2 == &ident)
-                {
-                    // infer type of expr and try to unify with def.ty
-                    let ty = ty::infer(
-                        &mut HashMap::new(),
-                        &mut NameSource::new(),
-                        &env.ty_env,
-                        &expr,
-                    )
-                    .map_err(|e| anyhow!("{}", e))?;
+        let response = eval_stm(&mut env, stm);
 
-                    if ty::unify(std::iter::once((ty.clone(), def.ty.clone()))).all(|x| x.is_ok()) {
-                        let result = eval(&env.env, expr)?;
-                        objs.push(result);
-                        tx.send(Ok(String::from("Inserted 1\n")))
-                            .context("Ack channel prematurely closed")?;
-                    } else {
-                        tx.send(Err(anyhow!(
-                            "Could not insert {:?} into table {:?} with definition {:?}\n",
-                            expr,
-                            ident,
-                            &def.ty
-                        )))
-                        .context("Ack channel prematurely closed")?;
-                    }
-                } else {
-                    tx.send(Err(anyhow!("No such table\n")))
-                        .context("Ack channel prematurely closed")?;
-                }
-            }
-            Statement::Select(ident) => {
-                if let Some((_, _, objs)) = tables.iter().find(|(ident2, _, _)| ident2 == &ident) {
-                    tx.send(Ok(format!("{:?}\n", objs)))?;
-                } else {
-                    tx.send(Err(anyhow!("No such table\n")))
-                        .context("Ack channel prematurely closed")?;
-                }
-            }
-            Statement::Let(ident, expr) => {
-                // infer type of expr and try to unify with def.ty
-                println!("got let");
-                let ty = ty::infer(
-                    &mut HashMap::new(),
-                    &mut NameSource::new(),
-                    &env.ty_env,
-                    &expr,
-                )
-                .map_err(|e| anyhow!("{}", e))
-                .unwrap();
-
-                println!("got scheme");
-                let scheme = ty::generalize(&env.ty_env, ty.clone());
-
-                env.ty_env.insert(ident.clone(), scheme);
-
-                println!("got obj");
-                let obj = eval(&env.env, expr)?;
-
-                env.env = env.env.insert(&ident, obj);
-
-                tx.send(Ok(format!("{}: {}\n", ident, ty)))?;
-            }
-        }
+        tx.send(response)
+            .context("Ack channel prematurely closed")?;
     }
 }
